4515c248cb7cc17732a9278004547d7c4169ffd9
Alireza Fathi <fathi.alireza701@gmail.com>
2025-07-11 15:32:00 +0330

    Renamed Macksclicks to hosna #6

--- web/src/modules/webConfig/app.ts
+++ web/src/modules/webConfig/app.ts
@@ -1,5 +1,5 @@
 export const app = {
-  name: 'maxclicks',
-  domain: 'maxclicks.ai',
+  name: 'Hosna',
+  domain: 'hosna.ir',
   version: '0.0.8', // Do not touch this mannually, it will be automatically set by the "npm version" command.
 } as const
